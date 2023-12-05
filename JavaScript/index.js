9dfa514afdbb590317398cc2ef73e1d8b9f7d0d7
Iamiran1 <vinicius.cezimbra.miranda@gmail.com>
2023-12-05 16:47:02 -0300

    chat

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //Sino
-    let mode = document.querySelector('.sino');
-    mode.setAttribute('src', './imagens/bell_off.png');
-    mode.addEventListener('click', function (event) {
-       if (mode.src.includes("imagens/bell_off.png")) {
-           mode.setAttribute('src', './imagens/bell_on.png');
-       } else {
-            mode.setAttribute('src', "./imagens/bell_off.png");
-       }
+    let meuInput = document.querySelector("body > footer > ul > li > input");
 
-   });
+    if (meuInput) {
+        meuInput.id = "seuInputId";
+        meuInput.name = "seuInputName";
+        meuInput.autocomplete = "off";
+
+        meuInput.addEventListener('input', function () {
+            localStorage.setItem('meuInputValue', meuInput.value);
+            console.log('Valor do input atualizado:', meuInput.value);
+        });
+
+        let valorSalvo = localStorage.getItem('meuInputValue');
+        if (valorSalvo) {
+            meuInput.value = valorSalvo;
+        }
+    }
+
+    meuInput.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            let ul = document.createElement('ul');
+            ul.className = 'btn2';
+            let p = document.createElement('p');
+            p.style.fontSize = '1rem';
+            p.textContent = meuInput.value;
+            ul.appendChild(p);
+            document.querySelector('main').appendChild(ul);
+            ul.style.position = 'absolute';
+            ul.style.top = '20rem';
+            ul.style.left = '15.5rem';
+            meuInput.value = '';
+            p = '';
+        }
+    });
 });
